8912f711b6b7ebf4aa233a658cf17d56f09df20b
silvermistshadow <bmead72@gmail.com>
2019-08-15 11:18:59 -0700

    update html and UI javascript

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-import { Triangle } from './triangle.js'
+import { Triangle } from './triangle.js';
+
+$(document).ready(function(){
+  $("#triangle-form").submit(function(event){
+    event.preventDefault();
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+    var newTriangle = new Triangle(side1, side2, side3);
+    var output = newTriangle.checkType();
+    $("#output").text(output);
+  });
 
 
+});
