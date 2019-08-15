7ab9c3a90913c588f44ed51a3001921e00733956
silvermistshadow <bmead72@gmail.com>
2019-08-15 09:06:01 -0700

    libraries, empty files, work on javascript and specs

---
+++ src/main.js
@@ -0,0 +1,3 @@
+import { triangleModule } from './triangle'
+
+
---
+++ src/triangle.js
@@ -0,0 +1,14 @@
+function Triangle(side1, side2, side3) {
+  this.side1 = side1;
+  this.side2 = side2;
+  this.side3 = side3;
+}
+
+Triangle.prototype.checkType = function() {
+  if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+    return "not a triangle";
+  }
+  console.log("conditional not reached.");
+};
+
+export const triangleModule = Triangle;
